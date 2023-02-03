7c2aa9eaf6bec1583ec65bd1094e3183a46ad2e7
Mihai Costea <macostea@users.noreply.github.com>
2023-02-03 14:43:53 +0200

    Simple protocol, multiple messages

--- client/src/main.rs
+++ client/src/main.rs
@@ -6,9 +6,14 @@ fn main() -> std::io::Result<()> {
   let mut s = [0; 64];
 
   let msg = "Hello world";
+  let msg2 = "Ping";
+
   let bytes = (msg.len() as u32).to_be_bytes();
   s[..4].clone_from_slice(&bytes);
   s[4..4 + msg.len()].clone_from_slice(msg.as_bytes());
+
+  s[4 + msg.len() .. 4 + msg.len() + 4].clone_from_slice(&(msg2.len() as u32).to_be_bytes());
+  s[4 + msg.len() + 4 .. 4 + msg.len() + 4 + msg2.len()].clone_from_slice(msg2.as_bytes());
 
   stream.write(&s)?;
   
--- server/src/main.rs
+++ server/src/main.rs
@@ -3,23 +3,37 @@ use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, error::Error};
 const MAX_MSG_SIZE: usize = 4096;
 const HEADER_SIZE: usize = 4;
 
-fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+fn read_single_message(mut stream: &TcpStream, start: usize) -> Result<Vec<u8>, Box<dyn Error>> {
     let mut buf = [0; 4 + MAX_MSG_SIZE + 1];
 
     // Read header
-    let header_size = stream.read(&mut buf[0 .. HEADER_SIZE])?;
+    let header_size = stream.read(&mut buf[start .. start + HEADER_SIZE])?;
 
     if header_size != HEADER_SIZE {
         return Err("Mismatch header size".into());
     }
 
     let mut msg_size: [u8; HEADER_SIZE] = [0; HEADER_SIZE];
-    msg_size.clone_from_slice(&buf[.. header_size]);
+    msg_size.clone_from_slice(&buf[start .. start + header_size]);
     let parsed_msg_size: usize = u32::from_be_bytes(msg_size).try_into()?;
 
-    let msg_size = stream.read(&mut buf[header_size .. header_size + parsed_msg_size])?;
+    if parsed_msg_size == 0 {
+        return Err("No more msg".into());
+    }
 
-    println!("Read message from client: {:?}", &buf[header_size .. header_size + msg_size]);
+    let msg_size = stream.read(&mut buf[start + header_size .. start + header_size + parsed_msg_size])?;
+
+    let msg = buf[start + header_size .. start + header_size + msg_size].to_vec();
+
+    Ok(msg)
+}
+
+fn handle_client(stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    let mut current_msg_start = 0;
+    while let Ok(res) = read_single_message(&stream, current_msg_start) {
+        println!("Read from client {:?}", res);
+        current_msg_start += res.len();
+    }
 
     Ok(())
 }
